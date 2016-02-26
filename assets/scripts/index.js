e94be455210be255167282d9f4d85ba21442cdc4
kpie89 <kpie89@gmail.com>
2016-02-26 17:49:34 -0500

    functioning random beer button

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,6 +11,32 @@ const myApp = {
 };
 
 $(document).ready(() => {
+  let displayBeer = function(response) {
+    let beer = response.beer;
+    console.log('here');
+    let beerListing = require('./beer_listing.handlebars');
+    console.log(beer);
+      $('.content').append(beerListing({
+         beer
+       }));
+  };
+
+  let randomBeer = function(){
+    $.ajax({
+      url: myApp.BASE_URL + '/beer/random',
+      method: 'GET',
+      dataType: 'json'
+    }).done(function(beer){
+      console.log(beer);
+      displayBeer(beer);
+    });
+  };
+  $('.random').on('click', function(e){
+    e.preventDefault();
+    randomBeer();
+  });
+
+
   console.log('here');
   $('#sign-up').on('submit', function(e) {
     e.preventDefault();
